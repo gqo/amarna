6a0f82ddcffc92983d286dc620486df071e8a4dd
Justin Wang <justin39@gmail.com>
2019-02-16 12:04:15 -0500

    Remove swp files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import {
-    BrowserRouter as Router,
     Route,
-    Link
 } from 'react-router-dom'
 import { Layout } from './Layout'
 import { Home } from './Home'
