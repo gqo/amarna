fef0e299b9681ce7b5cdb8fd1f047297cd8a8684
gqo <ggmferguson@gmail.com>
2019-02-16 00:17:38 -0500

    Add connection to mongodb in main

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -7,8 +7,14 @@ import (
 )
 
 func main() {
+	dsn := "mongodb://admin:basketorangenumberbleacher@amarna-shard-00-00-gcgag.gcp.mongodb.net:27017,amarna-shard-00-01-gcgag.gcp.mongodb.net:27017,amarna-shard-00-02-gcgag.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Amarna-shard-0&authSource=admin&retryWrites=true"
+	_, err := nosql.NewDatastore(dsn)
+	if err != nil {
+		log.Fatalln("Could not create datastore. Received err:", err)
+	}
+	log.Println("Connected to the datastore!")
+
 	log.Println("Starting Amarna backend...")
-	nosql.HelloWorld()
 	for {
 
 	}
