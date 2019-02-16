cc7eeeac1bfc1d3b83276e27d7c9e3a1d1f8d115
gqo <ggmferguson@gmail.com>
2019-02-16 12:46:35 -0500

    Add json tags to structs, wrap time.Time correctly

--- backend/pkg/entities/entities.go
+++ backend/pkg/entities/entities.go
@@ -1,38 +1,50 @@
 package entities
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
 
 // User defines the user in the Amarna service
 type User struct {
-	username   string
-	knownLangs []string
-	learnLangs []string
+	Username   string   `json:"username"`
+	KnownLangs []string `json:"known_langs"`
+	LearnLangs []string `json:"learn_langs"`
+}
+
+// JSONTime is a type alias so we can more easily deal with time in Go -> JSON
+type JSONTime time.Time
+
+// MarshalJSON lets you convert time.Time to JSON data
+func (t JSONTime) MarshalJSON() ([]byte, error) {
+	ts := fmt.Sprintf("\"%s\"", time.Time(t).Format("Mon Jan _2 15:04:05 2006"))
+	return []byte(ts), nil
 }
 
 // Letter defines the letters that have been sent between users
 type Letter struct {
-	from      string
-	to        string
-	timestamp time.Time
-	topic     string
-	week      int32
+	From      string   `json:"from"`
+	To        string   `json:"to"`
+	Timestamp JSONTime `json:"timestamp"`
+	Topic     string   `json:"topic"`
+	Week      int32    `json:"week"`
 }
 
 // Pairing defines pairs of users that have matched
 type Pairing struct {
-	leftUser  string
-	rightUser string
+	LeftUser  string `json:"left_user"`
+	RightUser string `json:"right_user"`
 }
 
 // Topic defines a topic for learning in Amarna
 type Topic struct {
-	title  string
-	length string
+	Title  string `json:"title"`
+	Length string `json:"length"`
 }
 
 // Section defines a section of a topic
 type Section struct {
-	topicTitle string
-	week       int32
-	desc       string
+	TopicTitle string `json:"topic_title"`
+	Week       int32  `json:"week"`
+	Desc       string `json:"desc"`
 }
