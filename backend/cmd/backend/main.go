ae9daad842d3eaccf2b53b7b195506634b725a77
Reece Rodriguez <rtr266@nyu.edu>
2019-02-16 23:10:48 -0500

    Add some work la di daa

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -5,134 +5,14 @@ import (
 	"flag"
 	"fmt"
 	"log"
-	"net"
 	"net/http"
 
 	jsonstruct "../../pkg/JSON-structs"
-	mysql "../../pkg/mysql"
+	storage "../../pkg/mysql"
 )
 
-//Packet will hold JSON information for later decoding
-type Packet struct {
-	Action   string `json:"action"`
-	CompName string `json:"compName"`
-}
-
-func handleRequest(c net.Conn) {
-	defer c.Close()
-	var header Packet
-	// we create a decoder that reads directly from the socket
-	decode := json.NewDecoder(c)
-
-	decodeErr := decode.Decode(&header)
-	if decodeErr != nil {
-		log.Println(decodeErr)
-	}
-
-	fmt.Printf("Action: %s, CompName: %s\n", header.Action, header.CompName)
-
-}
-
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello world!")
-
-}
-
-//ValidateUserHandler will handle decoding of JSON packages for user validation
-func ValidateUserHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "ValidateUser")
-	decoder := json.NewDecoder(r.Body)
-	var valUsrJSON jsonstruct.ValUser
-	valUsrErr := decoder.Decode(&valUsrJSON)
-	if valUsrErr != nil {
-		panic(valUsrErr)
-	}
-	log.Println(valUsrJSON)
-}
-
-//UpdateKnownLangsHandler will handle decoding of JSON pakcages for known language updating
-func UpdateKnownLangsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "UpdateKnownLangs")
-	decoder := json.NewDecoder(r.Body)
-	var updateKnownLangsJSON jsonstruct.UpdateKnownLangs
-	updateKnownLangsErr := decoder.Decode(&updateKnownLangsJSON)
-	if updateKnownLangsErr != nil {
-		panic(updateKnownLangsErr)
-	}
-	log.Println(updateKnownLangsJSON)
-}
-
-//UpdateLearnLangsHandler will handle decoding of JSON pakcages for new or learned language updating
-func UpdateLearnLangsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "UpdateLearnLangs")
-	decoder := json.NewDecoder(r.Body)
-	var updateLearnLangsJSON jsonstruct.UpdateLearnLangs
-	updateLearnLangsErr := decoder.Decode(&updateLearnLangsJSON)
-	if updateLearnLangsErr != nil {
-		panic(updateLearnLangsErr)
-	}
-	log.Println(updateLearnLangsJSON)
-}
-
-//InsertPairingHandler will handle decoding of JSON pakcages for pair inserting
-func InsertPairingHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "InsertPairing")
-	decoder := json.NewDecoder(r.Body)
-	var InsertPairingJSON jsonstruct.InsertPairing
-	InsertPairingErr := decoder.Decode(&InsertPairingJSON)
-	if InsertPairingErr != nil {
-		panic(InsertPairingErr)
-	}
-	log.Println(InsertPairingJSON)
-}
-
-//GetPairingsHandler will handle decoding of JSON pakcages for pairing retreival
-func GetPairingsHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "GetPairings")
-	decoder := json.NewDecoder(r.Body)
-	var GetPairingJSON jsonstruct.GetPairings
-	GetPairingErr := decoder.Decode(&GetPairingJSON)
-	if GetPairingErr != nil {
-		panic(GetPairingErr)
-	}
-	log.Println(GetPairingJSON)
-}
-
-//GetMatchesHandler will handle decoding of JSON pakcages for mathing retreival
-func GetMatchesHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "GetMatches")
-	decoder := json.NewDecoder(r.Body)
-	var GetMatchesJSON jsonstruct.GetMatches
-	GetMatchesErr := decoder.Decode(&GetMatchesJSON)
-	if GetMatchesErr != nil {
-		panic(GetMatchesErr)
-	}
-	log.Println(GetMatchesJSON)
-}
-
-//GetSectionHandler will handle decoding of JSON pakcages for section retreival
-func GetSectionHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "GetSection")
-	decoder := json.NewDecoder(r.Body)
-	var GetSectionJSON jsonstruct.GetSection
-	GetSectionErr := decoder.Decode(&GetSectionJSON)
-	if GetSectionErr != nil {
-		panic(GetSectionErr)
-	}
-	log.Println(GetSectionJSON)
-}
-
-//GetNextUncompletedSectionHandler will handle decoding of JSON pakcages for retreival of upcoming and uncompleted sections
-func GetNextUncompletedSectionHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "GetNextUncompletedSection")
-	decoder := json.NewDecoder(r.Body)
-	var GetNextUncompletedSectionJSON jsonstruct.GetNextUncompletedSection
-	GetNextUncompletedSectionErr := decoder.Decode(&GetNextUncompletedSectionJSON)
-	if GetNextUncompletedSectionErr != nil {
-		panic(GetNextUncompletedSectionErr)
-	}
-	log.Println(GetNextUncompletedSectionJSON)
-}
+var db storage.Datastore
+var response jsonstruct.JSONResponse
 
 func main() {
 	// var port = flag.Int("listen", 8081, "the port number") //default port is 8081
@@ -146,8 +26,11 @@ func main() {
 		log.Fatalln("You need to provide a DSN using the -dsn flag")
 	}
 
-	_, datastoreErr := mysql.NewDatastore(*dsn)
-	if datastoreErr != nil {
+	var err error
+
+	db, err = storage.NewDatastore(*dsn)
+
+	if err != nil {
 		log.Fatalln("Could not create datastore. Received err:", datastoreErr)
 	}
 	log.Println("Connected to the datastore!")
@@ -180,4 +63,168 @@ func main() {
 
 	// }
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
+
+}
+
+//ValidateUserHandler will handle decoding of JSON packages for user validation
+func ValidateUserHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ValidateUser")
+	decoder := json.NewDecoder(r.Body)
+	var valUsrJSON jsonstruct.ValUser
+	valUsrErr := decoder.Decode(&valUsrJSON)
+	if valUsrErr != nil {
+		log.Println(valUsrErr)
+	}
+	log.Println(valUsrJSON)
+	isValid, ValidateUserErr := db.ValidateUser(valUsrJSON.Username)
+
+	if ValidateUserErr != nil {
+		response = jsonstruct.JSONResponse{
+			Error:   ValidateUserErr,
+			Boolean: nil,
+			Section: nil,
+		}
+	} else {
+		response = jsonstruct.JSONResponse{
+			Error:   nil,
+			Boolean: isValid,
+			Section: nil,
+		}
+	}
+	respMarsh, marshErr := json.Marshal(response)
+	if marshErr != nil {
+		fmt.Println("There has been a marshalling error:", marshErr)
+	}
+	fmt.Fprintf(w, string(respMarsh))
+}
+
+//UpdateKnownLangsHandler will handle decoding of JSON pakcages for known language updating
+func UpdateKnownLangsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "UpdateKnownLangs")
+	decoder := json.NewDecoder(r.Body)
+	var updateKnownLangsJSON jsonstruct.UpdateKnownLangs
+	updateKnownLangsErr := decoder.Decode(&updateKnownLangsJSON)
+	if updateKnownLangsErr != nil {
+		log.Println(updateKnownLangsErr)
+	}
+	log.Println(updateKnownLangsJSON)
+	knownUpdateErr := db.UpdateKnownLangs(updateKnownLangsJSON.Username, updateKnownLangsJSON.KnownLangs)
+	if knownUpdateErr != nil {
+		response = jsonstruct.JSONResponse{
+			Error:   knownUpdateErr,
+			Boolean: nil,
+			Section: nil,
+		}
+	} else {
+		response = jsonstruct.JSONResponse{
+			Error:   nil,
+			Boolean: nil,
+			Section: nil,
+		}
+	}
+	respMarsh, marshErr := json.Marshal(response)
+	if marshErr != nil {
+		fmt.Println("There has been a marshalling error:", marshErr)
+	}
+	fmt.Fprintf(w, string(respMarsh))
+}
+
+//UpdateLearnLangsHandler will handle decoding of JSON pakcages for new or learned language updating
+func UpdateLearnLangsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "UpdateLearnLangs")
+	decoder := json.NewDecoder(r.Body)
+	var updateLearnLangsJSON jsonstruct.UpdateLearnLangs
+	updateLearnLangsErr := decoder.Decode(&updateLearnLangsJSON)
+	if updateLearnLangsErr != nil {
+		log.Println(updateLearnLangsErr)
+	}
+	log.Println(updateLearnLangsJSON)
+	learnUpdateErr := db.UpdateLearnLangs(updateLearnLangsJSON.Username, updateLearnLangsJSON.LearnLangs)
+	if learnUpdateErr != nil {
+		response = jsonstruct.JSONResponse{
+			Error:   learnUpdateErr,
+			Boolean: nil,
+			Section: nil,
+		}
+	} else {
+		response = jsonstruct.JSONResponse{
+			Error:   nil,
+			Boolean: nil,
+			Section: nil,
+		}
+	}
+	respMarsh, marshErr := json.Marshal(response)
+	if marshErr != nil {
+		fmt.Println("There has been a marshalling error:", marshErr)
+	}
+	fmt.Fprintf(w, string(respMarsh))
+}
+
+//InsertPairingHandler will handle decoding of JSON pakcages for pair inserting
+func InsertPairingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "InsertPairing")
+	decoder := json.NewDecoder(r.Body)
+	var InsertPairingJSON jsonstruct.InsertPairing
+	InsertPairingErr := decoder.Decode(&InsertPairingJSON)
+	if InsertPairingErr != nil {
+		log.Println(InsertPairingErr)
+	}
+	log.Println(InsertPairingJSON)
+	insertErr := db.InsertPairing(InsertPairingJSON.LeftUsername, InsertPairingJSON.RightUsername, InsertPairingJSON.LeftUserLang, InsertPairingJSON.RightUserLang)
+}
+
+//GetPairingsHandler will handle decoding of JSON pakcages for pairing retreival
+func GetPairingsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GetPairings")
+	decoder := json.NewDecoder(r.Body)
+	var GetPairingJSON jsonstruct.GetPairings
+	GetPairingErr := decoder.Decode(&GetPairingJSON)
+	if GetPairingErr != nil {
+		log.Println(GetPairingErr)
+	}
+	log.Println(GetPairingJSON)
+	db.GetPairings(GetPairingJSON.Username)
+}
+
+//GetMatchesHandler will handle decoding of JSON pakcages for mathing retreival
+func GetMatchesHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GetMatches")
+	decoder := json.NewDecoder(r.Body)
+	var GetMatchesJSON jsonstruct.GetMatches
+	GetMatchesErr := decoder.Decode(&GetMatchesJSON)
+	if GetMatchesErr != nil {
+		log.Println(GetMatchesErr)
+	}
+	log.Println(GetMatchesJSON)
+	db.GetMatches(GetMatchesJSON.User)
+}
+
+//GetSectionHandler will handle decoding of JSON pakcages for section retreival
+func GetSectionHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GetSection")
+	decoder := json.NewDecoder(r.Body)
+	var GetSectionJSON jsonstruct.GetSection
+	GetSectionErr := decoder.Decode(&GetSectionJSON)
+	if GetSectionErr != nil {
+		log.Println(GetSectionErr)
+	}
+	log.Println(GetSectionJSON)
+	db.GetSection(GetSectionJSON.TopicTitle, GetSectionJSON.TopicLang)
+}
+
+//GetNextUncompletedSectionHandler will handle decoding of JSON pakcages for retreival of upcoming and uncompleted sections
+func GetNextUncompletedSectionHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GetNextUncompletedSection")
+	decoder := json.NewDecoder(r.Body)
+	var GetNextUncompletedSectionJSON jsonstruct.GetNextUncompletedSection
+	GetNextUncompletedSectionErr := decoder.Decode(&GetNextUncompletedSectionJSON)
+	if GetNextUncompletedSectionErr != nil {
+		log.Println(GetNextUncompletedSectionErr)
+	}
+	log.Println(GetNextUncompletedSectionJSON)
+	db.GetNextUncompletedSection(GetNextUncompletedSectionJSON.LeftUsername, GetNextUncompletedSectionJSON.RightUsername, GetNextUncompletedSectionJSON.TopicTitle, GetNextUncompletedSectionJSON.TopicLang)
 }
--- backend/pkg/JSON-structs/JSON-stucts.go
+++ backend/pkg/JSON-structs/JSON-stucts.go
@@ -8,49 +8,56 @@ import (
 
 //ValUser defines struct for JSON packages recieved from frontend
 type ValUser struct {
-	username string
+	Username string
 }
 
 //UpdateKnownLangs defines struct for JSON packages recieved from frontend
 type UpdateKnownLangs struct {
-	username   string
-	knownLangs []string
+	Username   string
+	KnownLangs []string
 }
 
 //UpdateLearnLangs defines struct for JSON packages recieved from frontend
 type UpdateLearnLangs struct {
-	username   string
-	learnLangs []string
+	Username   string
+	LearnLangs []string
 }
 
 //InsertPairing defines struct for JSON packages recieved from frontend
 type InsertPairing struct {
-	leftUsername  string
-	leftUserLang  string
-	rightUsername string
-	rightUserLang string
+	LeftUsername  string
+	LeftUserLang  string
+	RightUsername string
+	RightUserLang string
 }
 
 //GetPairings defines struct for JSON packages recieved from frontend
 type GetPairings struct {
-	username string
+	Username string
 }
 
 //GetMatches defines struct for JSON packages recieved from frontend
 type GetMatches struct {
-	user entity.User
+	User entity.User
 }
 
 //GetSection defines struct for JSON packages recieved from frontend
 type GetSection struct {
-	topicTitle string
-	topicLang  string
+	TopicTitle string
+	TopicLang  string
 }
 
 //GetNextUncompletedSection defines struct for JSON packages recieved from frontend
 type GetNextUncompletedSection struct {
-	leftUsername  string
-	rightUsername string
-	topicTitle    string
-	topicLang     string
+	LeftUsername  string
+	RightUsername string
+	TopicTitle    string
+	TopicLang     string
+}
+
+//JSONResponse will the the JSON used to respond to the frontend
+type JSONResponse struct {
+	Error   error
+	Boolean *bool
+	Section *entity.Section
 }
