d7584d369688344c5c097140d81b321934bb468a
Justin Wang <justin39@gmail.com>
2019-02-17 02:54:11 -0500

    Fix cors issue

--- frontend/src/ReferencePanel.js
+++ frontend/src/ReferencePanel.js
@@ -9,11 +9,13 @@ export class ReferencePanel extends Component {
     render () {
         return (
             <Card className="lesson">
-                <h3>{this.props.lesson['title']}</h3>
-                <h4>{this.props.lesson['section']}</h4>
-                <p>
-                    {this.props.lesson['description']}
-                </p>
+                <div className="content">
+                    <h3>{this.props.lesson['title']}</h3>
+                    <h4>{this.props.lesson['section']}</h4>
+                    <p>
+                        {this.props.lesson['description']}
+                    </p>
+                </div>
                 <Button color="primary" onClick={this.props.onClick}>Request to continue</Button>
             </Card>
         );
