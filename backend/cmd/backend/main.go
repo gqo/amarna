06329340989e64df3a790b2c2da9daa23a4189bb
Reece Rodriguez <rtr266@nyu.edu>
2019-02-16 19:18:34 -0500

    Mock out handlers

--- backend/cmd/backend/main.go
+++ backend/cmd/backend/main.go
@@ -6,7 +6,7 @@ import (
 	"fmt"
 	"log"
 	"net"
-	"strconv"
+	"net/http"
 
 	mysql "../../pkg/mysql"
 )
@@ -32,8 +32,36 @@ func handleRequest(c net.Conn) {
 
 }
 
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello world!")
+}
+func ValidateUserHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "ValidateUser")
+}
+func UpdateKnownLangsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "UpdateKnownLangs")
+}
+func UpdateLearnLangsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "UpdateLearnLangs")
+}
+func InsertPairingHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "InsertPairing")
+}
+func GetPairingsHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GetPairings")
+}
+func GetMatchesHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GetMatches")
+}
+func GetSectionHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GetSection")
+}
+func GetNextUncompletedSectionHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "GetNextUncompletedSection")
+}
+
 func main() {
-	var port = flag.Int("listen", 8081, "the port number") //default port is 8081
+	// var port = flag.Int("listen", 8081, "the port number") //default port is 8081
 	var dsn = flag.String("dsn", "", "sets data source name for backend server")
 
 	flag.Parse()
@@ -48,21 +76,32 @@ func main() {
 	}
 	log.Println("Connected to the datastore!")
 
-	log.Println("Opening client connections on port", *port)
-	l, listenErr := net.Listen("tcp", ":"+strconv.Itoa(*port))
-	if listenErr != nil {
-		log.Fatalln("Could not listen to tcp port. Received err:", listenErr)
-	}
-	log.Println("Opened client connections!")
+	// log.Println("Opening client connections on port", *port)
+	// l, listenErr := net.Listen("tcp", ":"+strconv.Itoa(*port))
+	// if listenErr != nil {
+	// 	log.Fatalln("Could not listen to tcp port. Received err:", listenErr)
+	// }
+	// log.Println("Opened client connections!")
+
+	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/ValidateUser", ValidateUserHandler)
+	http.HandleFunc("/UpdateKnownLangs", UpdateKnownLangsHandler)
+	http.HandleFunc("/UpdateLearnLangs", UpdateLearnLangsHandler)
+	http.HandleFunc("/ InsertPairing", InsertPairingHandler)
+	http.HandleFunc("/GetPairings", GetPairingsHandler)
+	http.HandleFunc("/GetMatches", GetMatchesHandler)
+	http.HandleFunc("/GetSection", GetSectionHandler)
+	http.HandleFunc("/GetNextUncompletedSection", GetNextUncompletedSectionHandler)
 
 	log.Println("Starting Amarna backend...")
-	for {
-		conn, connErr := l.Accept()
-		if connErr != nil {
-			log.Println(connErr)
-		}
+	// for {
+	// 	conn, connErr := l.Accept()
+	// 	if connErr != nil {
+	// 		log.Println(connErr)
+	// 	}
 
-		go handleRequest(conn)
+	// 	go handleRequest(conn)
 
-	}
+	// }
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
